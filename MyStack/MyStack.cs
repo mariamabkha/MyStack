5b9c4f7a2d6f75e146fadaccad0e6afbd1577e81
Mariam Abkhazava <85383398+mariamabxa@users.noreply.github.com>
2023-04-12 20:30:00 +0400

    add custom stack files

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,67 @@
+﻿public class MyStack<T>
+{
+    private List<T> list;    // Private field to store the elements of the stack using a List<T> 
+
+    public MyStack()
+    {
+        list = new List<T>();   
+    }
+
+    // Property to get count of elements in stack
+    public int Count
+    {
+        get { return list.Count; } // Uses the Count property of List<T> to get the number of elements
+    }
+
+    // push an item onto the top of the stack
+    public void Push(T item)
+    {
+        list.Add(item);       // Adds the item to the end of the List<T> using the Add() method
+    }
+
+    // Method to pop the top item from the stack and return it
+    public T Pop()
+    {
+        if (list.Count > 0)   // Checks if the stack is not empty
+        {
+            T obj = list[list.Count - 1];   // Retrieves the top element from the List<T> using the last index
+            list.RemoveAt(list.Count - 1);  // Removes top element from the List<T>
+            return obj;                     // Returns popped element
+        }
+        else
+        {
+            throw new InvalidOperationException("Stack is empty");  //  if stack is empty it throws exception
+        }
+    }
+
+    // clear all elements from the stack
+    public void Clear()
+    {
+        list.Clear();   
+    }
+
+    // peek at the top element in the stack without removing it
+    public T Peek()
+    {
+        if (list.Count > 0)   
+        {
+            return list[list.Count - 1];   // Retrieves the top element from the List<T> using the last index
+        }
+        else
+        {
+            throw new InvalidOperationException("Stack is empty");  // Throws an exception if the stack is empty
+        }
+    }
+
+    // print the stack elements and the number of elements in the stack
+    public void PrintStack()
+    {
+        Console.WriteLine("\nStack elements:");
+        foreach (T item in list)
+        {
+            Console.WriteLine(item);    // Prints each element in the stack
+        }
+        Console.WriteLine("Number of elements in the stack: " + Count);  // Prints the count of elements in the stack
+    }
+}
+
---
+++ MyStack/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyStack<int> stack = new MyStack<int>();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        stack.Push(5);
+
+        Console.WriteLine("stack elements:");
+        stack.PrintStack();
+
+        // Pop and print 
+        Console.WriteLine("\nPop elements from the stack:");
+        while (stack.Count > 0)
+        {
+            int item = stack.Pop();
+            Console.WriteLine("Popped : " + item);
+        }
+
+        // Try to peek at the top element when the stack is empty - throws exception
+        try
+        {
+            int peekItem = stack.Peek();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("\nException: " + ex.Message);
+        }
+
+        // Try to pop - throws exception
+        try
+        {
+            int popItem = stack.Pop();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+
+        // Clear and print the remaining elements
+        stack.Clear();
+        Console.WriteLine("\nStack elements after clearing:");
+        stack.PrintStack();
+
+        // Push new elements onto the stack
+        stack.Push(6);
+        stack.Push(7);
+        stack.Push(8);
+
+        Console.WriteLine("\nnew stack elements:");
+        stack.PrintStack();
+
+        // Peek at the top element without popping it
+        int topItem = stack.Peek();
+        Console.WriteLine("\nPeeked item: " + topItem);
+
+        // Print the final count of elements in the stack
+        Console.WriteLine("\nFinal count of elements in the stack: " + stack.Count);
+
+    }
+}
+
+
+
